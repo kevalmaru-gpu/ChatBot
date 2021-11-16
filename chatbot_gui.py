3cf168213e1a569f566bb35a3350da8b48c2bbaa
Savan Mandora <86867589+savanmandora@users.noreply.github.com>
2021-11-16 10:44:38 +0530

    Add files via upload
    new method of making Chatbot added

---
+++ chatbot_gui.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+root = Tk()
+root.configure(background="black")
+
+
+def send():
+    message = "You ->" + e.get()
+    txt.insert(END, "\n" + message)
+
+    if e.get() == 'hi' or e.get() == 'hello' or e.get() == 'my animal is not ok' :
+        txt.insert(END, "\n"+"Bot -> Hi, How can i help you ?")
+    elif e.get() == 'i need help':
+        txt.insert(END, "\n"+"Bot -> Ok, Please Enter your name.")
+    else:
+        txt.insert(END, "\n" + "Bot -> Sorry I didn't get you")
+
+    e.delete(0, END)
+
+
+
+txt = Text(root)
+txt.grid(row=0, column=0, columnspan=2)
+e = Entry(root, width=100)
+send_button = Button(root, text="send", command=send).grid(row=1, column=1)
+
+e.grid(row=1, column=0)
+root.title("ChatBot")
+
+root.mainloop()
