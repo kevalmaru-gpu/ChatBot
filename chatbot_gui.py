f62119a33794642dbe129e42055cf8798cebfae2
kevalCoder9106 <kevalcoder@gmail.com>
2021-11-17 21:13:27 +0530

    Applied my logic

--- chatbot_gui.py
+++ chatbot_gui.py
@@ -2,14 +2,12 @@
 # mongodb+srv://admin992:8733981820@cluster0.mgtom.mongodb.net/prj1?retryWrites=true&w=majority
 
 from tkinter import *
-
-from pymongo import MongoClient
+from typing import List
+from pymongo import MongoClient, results
 
 #Step 1: Connect to MongoDB - Note: Change connection string as needed
 client = MongoClient("mongodb+srv://admin992:8733981820@cluster0.mgtom.mongodb.net/prj1?retryWrites=true&w=majority")
 db = client.business
-
-
 
 user_info = {
     "name": "",
@@ -19,34 +17,133 @@ user_info = {
     "a_symptoms": "",
 }
 
+grammer_world = ["my","is","a","an","are","not","like","name","number","address","animal","symptoms","symtoms",","," "]
+
+def find_word(to_find,array):
+    """
+        this function will return any perticular word from list of string
+    """
+    for i in array:
+        if i == to_find:
+            return True
+
+    return False
+
+def differ_entity(array):
+    """
+        this function will return any word or list of word which are not grammetical and which could be our user data
+    """
+    is_differ = None
+    entities = []
+
+    for i in array:
+        for y in grammer_world:
+            if i == y:
+                is_differ = False
+        
+        if is_differ == True:
+            entities.append(i)
+        
+        is_differ = True
+
+    if len(entities) == 1:
+        return entities[0]
+    elif len(entities) == 0:
+        return 0
+    elif len(entities) > 1:
+        return entities
+
+
 def send(info):    
     message = "You ->" + e.get()
     txt.insert(END, "\n" + message)
 
     # convert the input text to lower case for case issues
     input_text = str(e.get()).lower()
+    # spliting input sentence into array
+    input_text_array = input_text.split(" ")
 
     if e.get() == 'hi' or e.get() == 'hello' or e.get() == 'my animal is not ok' or e.get() == 'i need help':
         txt.insert(END, "\n"+"Bot -> Please Enter your name.")
-    elif input_text.startswith("my name is"):
-        info["name"] = str(e.get()).split(" ",3)[3]
-        txt.insert(END, "\n"+f"Bot -> Ok, please enter your number.")
-    elif input_text.startswith("my number is"):
-        info["number"] = str(e.get()).split(" ",3)[3]
-        txt.insert(END, "\n"+f"Bot -> Ok, please enter your address.")
-    elif input_text.startswith("my address is"):
-        info["address"] = str(e.get()).split(" ",3)[3]
-        txt.insert(END, "\n"+f"Bot -> Ok, what is your animal name?")
-    elif input_text.startswith("my animal name is"):
-        info["a_name"] = str(e.get()).split(" ",4)[4]
-        txt.insert(END, "\n"+f"Bot -> Ok, what are the symptoms?")
-    elif input_text.startswith("symptoms are,"):
-        info["a_symptoms"] = str(e.get()).split(" ",2)[2]
+    elif find_word("name",input_text_array) and find_word("animal",input_text_array) == False:
+        # getting user info
+        differed_entity = differ_entity(input_text_array)
+
+        if differed_entity != 0 and type(differed_entity) != list: # checking if it isnt a list
+            # directly add to dict
+            info["name"] = differed_entity 
+
+            txt.insert(END, "\n"+f"Bot -> Ok, please enter your number.")
+            print(differed_entity)
+        elif differed_entity != 0 and type(differed_entity) == list: # if it is a list
+            final_sentence = ""
+            # adding all words of list with space in between
+            for i in differed_entity:
+                final_sentence = final_sentence + " " + i
+            info["name"] = final_sentence.lstrip()
+            txt.insert(END, "\n"+f"Bot -> Ok, please enter your number.")
+            print(info["name"])
+        else:
+            txt.insert(END, "\n"+f"Bot -> Invalid response.")
+
+    elif find_word("number",input_text_array):
+        differed_entity = differ_entity(input_text_array)
+
+        if differed_entity != 0:
+            info["number"] = differed_entity
+            txt.insert(END, "\n"+f"Bot -> Ok, please enter your address.")
+            print(differed_entity)
+        else:
+            txt.insert(END, "\n"+f"Bot -> Invalid response.")
+
+    elif find_word("address",input_text_array):
+        differed_entity = differ_entity(input_text_array)
+
+        if differed_entity != 0 and type(differed_entity) != list:
+            info["address"] = differed_entity
+            txt.insert(END, "\n"+f"Bot -> Ok, what is your animal name?")
+            print(differed_entity)
+        elif differed_entity != 0 and type(differed_entity) == list:
+            final_sentence = ""
+            for i in differed_entity:
+                final_sentence = final_sentence + " " + i
+            info["address"] = final_sentence.lstrip()
+            txt.insert(END, "\n"+f"Bot -> Ok, what is your animal name?")
+            print(final_sentence)
+        else:
+            txt.insert(END, "\n"+f"Bot -> Invalid response.")
+
+    elif (find_word("animal",input_text_array) and find_word("is",input_text_array)):
+        differed_entity = differ_entity(input_text_array)
+
+        if differed_entity != 0:
+            info["a_name"] = differed_entity
+            txt.insert(END, "\n"+f"Bot -> Ok, what is your animal symptoms?")
+            print(differed_entity)
+        else:
+            txt.insert(END, "\n"+f"Bot -> Invalid response.")
+            
+    elif find_word("symptoms",input_text_array) or find_word("symptoms",input_text_array) or find_word("symtom"):
         result=db.chatbot_user.insert_one(user_info)
+        differed_entity = differ_entity(input_text_array)
+
+        if differed_entity != 0 and type(differed_entity) != list:
+            info["a_symptoms"] = differed_entity
+            print(differed_entity)
+        elif differed_entity != 0 and type(differed_entity) == list:
+            final_sentence = ""
+            for i in differed_entity:
+                final_sentence = final_sentence + " " + i
+            info["a_symptoms"] = final_sentence.lstrip()
+            print(final_sentence)
+        else:
+            txt.insert(END, "\n"+f"Bot -> Invalid response.")
+            return
 
         if info["name"] == "" or info["number"] == "" or info["address"] == "" or info["a_name"] == "" or info["a_symptoms"] == "":
             txt.insert(END, "\n"+f"Bot -> You have not given us some of your information.")
         else:
+            # conversation will end here
             txt.insert(END, "\n"+f"Bot -> Ok, this is your solution.")
             print(info)
     else:
